503d32c4cb751fb52253d78e0370f676769d3a44
Joram Visser <joram.n.v@gmail.com>
2018-04-28 23:11:57 +0200

    kotlin wip 7

---
+++ src/main/java/com/sparetimedevs/questlog/RootResourceProcessor.kt
@@ -0,0 +1,17 @@
+package com.sparetimedevs.questlog
+
+import com.sparetimedevs.questlog.login.LoginController
+import org.springframework.data.rest.webmvc.RepositoryLinksResource
+import org.springframework.hateoas.ResourceProcessor
+import org.springframework.stereotype.Component
+
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
+
+@Component
+class RootResourceProcessor : ResourceProcessor<RepositoryLinksResource> {
+
+	override fun process(resource: RepositoryLinksResource): RepositoryLinksResource {
+		resource.add(linkTo(LoginController::class.java).withRel("login"))
+		return resource
+	}
+}
