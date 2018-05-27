c4023f437bb05ef0c2af6d18082f5f5cbff0bb16
Joram Visser <joram.n.v@gmail.com>
2018-05-27 15:16:45 +0200

    wip 15

--- src/test/java/com/sparetimedevs/questlog/user/UserServiceTest.kt
+++ src/test/java/com/sparetimedevs/questlog/user/UserServiceTest.kt
@@ -1,47 +1,39 @@
 package com.sparetimedevs.questlog.user
 
 import com.sparetimedevs.questlog.login.Login
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.equalTo
-import org.junit.Assert.assertThat
-import org.junit.jupiter.api.Test
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import testsetup.EMAIL_ADDRESS_1
 import testsetup.PASSWORD_1
 import testsetup.userId1
-import testsupport.MockitoExtension
 import java.util.Optional
 
-@ExtendWith(MockitoExtension::class)
-internal class UserServiceTest {
+class UserServiceTest: StringSpec() {
 
-	@InjectMocks
-	private lateinit var userService: UserService
+	init {
 
-	@Mock
-	private lateinit var userRepository: UserRepository
+		val userRepository: UserRepository = Mockito.mock(UserRepository::class.java)
+		val userService = UserService(userRepository)
 
-	private val user = User(userId1, EMAIL_ADDRESS_1)
-	private val login = Login(user.emailAddress, PASSWORD_1)
+		val user = User(userId1, EMAIL_ADDRESS_1)
+		val login = Login(user.emailAddress, PASSWORD_1)
 
-	@Test
-	internal fun givenLoginWithEmailAddressThatIsFindableWhenGetUserIdReturnsUsersId() {
-		`when`(userRepository.findByEmailAddress(login.emailAddress)).thenReturn(Optional.of(user))
+		"givenLoginWithEmailAddressThatIsFindableWhenGetUserIdReturnsUsersId" {
+			`when`(userRepository.findByEmailAddress(login.emailAddress)).thenReturn(Optional.of(user))
 
-		val userId = userService.getUserId(login)
-		assertThat(userId, `is`(equalTo(userId1)))
-	}
+			val userId = userService.getUserId(login)
+			userId shouldBe userId1
+		}
 
-	@Test
-	internal fun givenLoginWithEmailAddressThatIsNotFindableWhenGetUserIsThrowsRuntimeException() {
-		`when`(userRepository.findByEmailAddress(login.emailAddress)).thenReturn(Optional.empty())
+		"givenLoginWithEmailAddressThatIsNotFindableWhenGetUserIsThrowsRuntimeException" {
+			`when`(userRepository.findByEmailAddress(login.emailAddress)).thenReturn(Optional.empty())
 
-		assertThrows<RuntimeException> {
-			userService.getUserId(login)
+			assertThrows<RuntimeException> {
+				userService.getUserId(login)
+			}
 		}
 	}
 }
